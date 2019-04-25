beb5e046eac627ebe389627a30ffd39fb0577a83
songkiyeon <proxima4311@gmail.com>
2019-04-26 03:48:11 +0900

    로그인 버튼 누를시 나타나도록 구현

--- landing-page/js/login.js
+++ landing-page/js/login.js
@@ -1,4 +1,4 @@
-function active_login(){
-    
-
+function active_login(id1,id2){
+    document.getElementById(id1).style.display = "none";
+    document.getElementById(id2).style.display = "block";
 }
