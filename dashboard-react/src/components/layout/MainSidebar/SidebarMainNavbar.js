fb593500cb4d1e4eda416a52e7568a77728dc323
Park <pjw9195@naver.com>
2019-04-30 23:30:18 +0900

    세부 desgin

--- dashboard-react/src/components/layout/MainSidebar/SidebarMainNavbar.js
+++ dashboard-react/src/components/layout/MainSidebar/SidebarMainNavbar.js
@@ -20,22 +20,15 @@ class SidebarMainNavbar extends React.Component {
     return (
       <div className="main-navbar">
         <Navbar
-          className="align-items-stretch bg-dark flex-md-nowrap border-bottom p-0"
+          className="align-items-stretch bg-dark flex-md-nowrap p-0"
           type="dark">
-          <NavbarBrand
-            className="w-100 mr-0"
-            href="#"
-            style={{ lineHeight: '25px' }}>
-            <div className="d-table m-auto">
-              <img
-                id="main-logo"
-                className="d-inline-block align-top mr-1"
-                style={{ height: '100%' }}
-                src={require('../../../assets/images/trimm.PNG')}
-                alt="Shards Dashboard"
-              />
-            </div>
-          </NavbarBrand>
+          <img
+            id="main-logo"
+            className="d-inline-block align-top mr-1"
+            style={{ width: '100%', height: 'auto' }}
+            src={require('../../../assets/images/trimm.PNG')}
+            alt="Shards Dashboard"
+          />
           {/* eslint-disable-next-line */}
           <a
             className="toggle-sidebar d-sm-inline d-md-none d-lg-none"
