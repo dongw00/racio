6423c62ee047647b2a205f3146e15287c6e1ab76
songkiyeon <proxima4311@gmail.com>
2019-06-03 22:21:42 +0900

    UI수정했습니다.. 이미지 변경 및 동적인 요소 많이 추가..

--- landing-page/js/language.js
+++ landing-page/js/language.js
@@ -5,24 +5,24 @@ function ChangeLanguage() {
     document.getElementById('text_login').innerHTML = '<i class="fa fa-sign-in mr-2"></i>Login';
     document.getElementById('text1').innerHTML =
       'Experience Web Application for TrimmOne!, <br> You can manage a data efficiently and useful';
-    document.getElementById('tag0').innerHTML = '&nbspHome';
+    document.getElementById('tag0').innerHTML = 'Home';
     // document.getElementById('tag1').innerHTML = 'Ranking & Achievement';
     // document.getElementById('tag2').innerHTML = 'Database';
-    document.getElementById('tag3').innerHTML = '&nbspOur Product';
-    document.getElementById('tag4').innerHTML = '&nbspOur Services';
+    document.getElementById('tag3').innerHTML = 'Our Product';
+    document.getElementById('tag4').innerHTML = 'Our Services';
     document.getElementById('text_fit0').innerHTML = 'Features';
-    document.getElementById('text_fit1').innerHTML = 'Comfortable';
-    document.getElementById('text_fit2').innerHTML = 'Securitiy';
-    document.getElementById('text_fit3').innerHTML = 'Data sharing';
-    document.getElementById('text_fit4').innerHTML = 'Easily access Anyware!';
-    document.getElementById('text_fit1_sub').innerHTML =
-      'Simple and clean UI, It is Easy to use';
-    document.getElementById('text_fit2_sub').innerHTML =
-      'We Protect personal information thoroughly and completely';
-    document.getElementById('text_fit3_sub').innerHTML =
-      'You can manage your data through the file system';
-    document.getElementById('text_fit4_sub').innerHTML =
-      'Anytime, anywhere, you can enjoy trimm One!';
+    // document.getElementById('text_fit1').innerHTML = 'Comfortable';
+    // document.getElementById('text_fit2').innerHTML = 'Securitiy';
+    // document.getElementById('text_fit3').innerHTML = 'Data sharing';
+    // document.getElementById('text_fit4').innerHTML = 'Easily access Anyware!';
+    // document.getElementById('text_fit1_sub').innerHTML =
+    //   'Simple and clean UI, It is Easy to use';
+    // document.getElementById('text_fit2_sub').innerHTML =
+    //   'We Protect personal information thoroughly and completely';
+    // document.getElementById('text_fit3_sub').innerHTML =
+    //   'You can manage your data through the file system';
+    // document.getElementById('text_fit4_sub').innerHTML =
+      // 'Anytime, anywhere, you can enjoy trimm One!';
     document.getElementById('post_text').innerHTML = 'Latest Posts';
 
     language = 'EN';
@@ -30,24 +30,24 @@ function ChangeLanguage() {
     document.getElementById('text_login').innerHTML = '<i class="fa fa-sign-in mr-2"></i>로그인';
     document.getElementById('text1').innerHTML =
     '트림원을 위한 웹 어플리케이션을 경험해보세요. <br/> 더 나은 삶을 경험해볼 준비가 되셨나요?';
-    document.getElementById('tag0').innerHTML = '&nbsp홈';
+    document.getElementById('tag0').innerHTML = '홈';
     // document.getElementById('tag1').innerHTML = '랭킹 & 업적';
     // document.getElementById('tag2').innerHTML = '데이터베이스';
-    document.getElementById('tag3').innerHTML = '&nbsp제품 설명';
-    document.getElementById('tag4').innerHTML = '&nbsp기타 서비스';
-    document.getElementById('text_fit0').innerHTML = 'trimm만의 특징';
-    document.getElementById('text_fit1').innerHTML = '편리함';
-    document.getElementById('text_fit2').innerHTML = '보안성';
-    document.getElementById('text_fit3').innerHTML = '데이터 공유';
-    document.getElementById('text_fit4').innerHTML = '어디서나 쉽게!';
-    document.getElementById('text_fit1_sub').innerHTML =
-      '누구나 쉽게 사용할 수 있는 간편하고 깔끔한 UI';
-    document.getElementById('text_fit2_sub').innerHTML =
-      '철저하고 완벽하게 개인 정보 보호';
-    document.getElementById('text_fit3_sub').innerHTML =
-      '파일 시스템을 통한 데이터 관리';
-    document.getElementById('text_fit4_sub').innerHTML =
-      '언제 어디서나 사용할 수 있는 trimm One!';
+    document.getElementById('tag3').innerHTML = '제품 설명';
+    document.getElementById('tag4').innerHTML = '기타 서비스';
+    document.getElementById('text_fit0').innerHTML = 'Trimm One 소개';
+    // document.getElementById('text_fit1').innerHTML = '편리함';
+    // document.getElementById('text_fit2').innerHTML = '보안성';
+    // document.getElementById('text_fit3').innerHTML = '데이터 공유';
+    // document.getElementById('text_fit4').innerHTML = '어디서나 쉽게!';
+    // document.getElementById('text_fit1_sub').innerHTML =
+    //   '누구나 쉽게 사용할 수 있는 간편하고 깔끔한 UI';
+    // document.getElementById('text_fit2_sub').innerHTML =
+    //   '철저하고 완벽하게 개인 정보 보호';
+    // document.getElementById('text_fit3_sub').innerHTML =
+    //   '파일 시스템을 통한 데이터 관리';
+    // document.getElementById('text_fit4_sub').innerHTML =
+    //   '언제 어디서나 사용할 수 있는 trimm One!';
     document.getElementById('post_text').innerHTML = '최근 게시물';
 
     language = 'KR';
--- landing-page/js/menu.js
+++ landing-page/js/menu.js
@@ -5,3 +5,56 @@ function toggleNav(num){
         document.getElementById('sidebar-wrapper').classList.remove('activation');
     }
 }
+
+
+//hover
+jQuery('#table_btn2').on('mouseenter', function() {
+    jQuery('#table2').addClass('drag');
+});
+jQuery('#table_btn3').on('mouseenter', function() {
+    jQuery('#table3').addClass('drag');
+});
+jQuery('#table_btn4').on('mouseenter', function() {
+    jQuery('#table4').addClass('drag');
+});
+jQuery('#table_btn5').on('mouseenter', function() {
+    jQuery('#table5').addClass('drag');
+});
+jQuery('#table_btn6').on('mouseenter', function() {
+    jQuery('#table6').addClass('drag');
+});
+jQuery('#table_btn7').on('mouseenter', function() {
+    jQuery('#table7').addClass('drag');
+});
+jQuery('#table_btn8').on('mouseenter', function() {
+    jQuery('#table8').addClass('drag');
+});
+jQuery('#table_btn9').on('mouseenter', function() {
+    jQuery('#table9').addClass('drag');
+});
+
+//out
+jQuery('#table_btn2').on('mouseleave', function() {
+    jQuery('#table2').removeClass('drag');
+});
+jQuery('#table_btn3').on('mouseleave', function() {
+    jQuery('#table3').removeClass('drag');
+});
+jQuery('#table_btn4').on('mouseleave', function() {
+    jQuery('#table4').removeClass('drag');
+});
+jQuery('#table_btn5').on('mouseleave', function() {
+    jQuery('#table5').removeClass('drag');
+});
+jQuery('#table_btn6').on('mouseleave', function() {
+    jQuery('#table6').removeClass('drag');
+});
+jQuery('#table_btn7').on('mouseleave', function() {
+    jQuery('#table7').removeClass('drag');
+});
+jQuery('#table_btn8').on('mouseleave', function() {
+    jQuery('#table8').removeClass('drag');
+});
+jQuery('#table_btn9').on('mouseleave', function() {
+    jQuery('#table9').removeClass('drag');
+});
